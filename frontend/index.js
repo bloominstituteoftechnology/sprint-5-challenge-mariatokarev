278e0807f9fb02704b37bf5bd3ac43db2905b023
mariatokarev <144080022+mariatokarev@users.noreply.github.com>
2023-09-30 11:26:59 -0400

    Add files via upload

--- frontend/index.js
+++ frontend/index.js
@@ -1,72 +1,85 @@
- function sprintChallenge5() { // Note the async keyword, in case you wish to use `await` inside sprintChallenge5
+async function sprintChallenge5() { // Note the async keyword, in case you wish to use `await` inside sprintChallenge5
   // 👇 WORK WORK BELOW THIS LINE 👇
  
-  const footer = document.querySelector('footer')
-  const currentYear = new Date().getFullYear()
-  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
-  fetch(' http://localhost:3003/api/learners')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
-  fetch(' http://localhost:3003/api/mentors')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+  const learnersResponse = await fetch('http://localhost:3003/api/learners');
+  const learnersData = await learnersResponse.json();
 
   
-  const cards = document.querySelectorAll('section > div');
-    cards.forEach((card) => {
-      card.classList.add('card');
-    });
+  const mentorsResponse = await fetch('http://localhost:3003/api/mentors');
+  const mentorsData = await mentorsResponse.json();
 
-    function buildLearnerCard(learner, mentors) {
-      const card = document.createElement('div');
-      card.classList.add('learner-card');
 
-      const nameP = document.createElement('h3');
-      nameP.textContent = `${learner.name}`;
-
-      const idElement = document.createElement('h3');
-      idElement.textContent = `${learner.id}`;
-
-      const emailP = document.createElement('div');
-      emailP.textContent = `${learner.email}`;
-
-      const mentorP = document.createElement('li');
-      mentorP.textContent = `${mentors}`;
-
-      [nameP, idElement, emailP, mentorP].forEach(() => {
-        card.appendChild();
-      });
-
-      card.addEventListener('click', async evt => {
-        document.querySelectorAll('.learner-card').forEach(card =>{
-          card.classList.remove('selected');
+ 
+      const cards = document.querySelector('.cards');
+      let data = []; 
+    
+      data.forEach(learner => {
+      
+        const newObject = {
+          learnerId: learner.id,
+          learnerFullName: learner.name,
+          email: learner.email,
+          mentorsArray: []
+        };
+    
+       
+        learner.mentors.forEach(mentor => {
+          const mentorName = `${mentor.firstName} ${mentor.lastName}`;
+          newObject.mentorsArray.push(mentorName);
         });
-        card.classList.add('active')
-      })
-
-      return card;
+    
+        
+        data.push(newObject);
+    
+        const learnerCard = buildLearnerCard(newObject);
+        cards.appendChild(learnerCard);
+      });
+    
+      function buildLearnerCard(learner) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+    
+        const nameP = document.createElement('h3');
+        nameP.textContent = learner.learnerFullName;
+    
+        const idElement = document.createElement('h3');
+        idElement.textContent = learner.learnerId;
+    
+        const emailP = document.createElement('div');
+        emailP.textContent = learner.email;
+    
+        const mentorList = document.createElement('h4');
+        learner.mentorsArray.forEach(mentorName => {
+          const mentorItem = document.createElement('ul');
+          mentorItem.textContent = mentorName;
+          mentorList.appendChild(mentorItem);
+        });
+    
+        [nameP, idElement, emailP, mentorList].forEach(element => {
+          card.appendChild(element);
+        });
+    
+        card.addEventListener('click', () => {
+          document.querySelectorAll('.learner-card').forEach(card => {
+            card.classList.remove('selected');
+          });
+          card.classList.add('selected');
+        });
+    
+        return card;
+      }
+    
+      document.addEventListener('DOMContentLoaded', () => {
+        data = fetchData(); 
+        data.forEach(learner => {
+          const learnerCard = buildLearnerCard(learner);
+          cards.appendChild(learnerCard);
+        });
+      });
     }
+    
+    // ❗ DO NOT CHANGE THE CODE BELOW
+    if (typeof module !== 'undefined' && module.exports) module.exports = { sprintChallenge5 };
+    else sprintChallenge5();
+    
 
-   
-   
-
-   
-    learners.forEach((learner) => {
-      const mentor = mentors.find((mentor) => mentor.id === learner.mentorId);
-      const learnerCard = buildLearnerCard(learner, mentor.name);
-      document.querySelector('section').appendChild(learnerCard);
-    });
-      const mentor = mentors.find((mentor) => mentor.id === learner.mentorId);
-      const learnerCard = buildLearnerCard(learner, mentor.name);
-      document.querySelector('section').appendChild(learnerCard);
-    }
-
-
-  // 👆 WORK WORK ABOVE THIS LINE 👆
-
-
-// ❗ DO NOT CHANGE THE CODE  BELOW
-if (typeof module !== 'undefined' && module.exports) module.exports = { sprintChallenge5 }
-else sprintChallenge5()
